ec621ef58c7318bebb690726c01246033b45d8ed
DESKTOP-C2ROD82\acade <academix128@gmail.com>
2021-03-18 00:26:38 +0900

    2021-03-17 과제 : 랜덤으로 학생 뽑기

--- src/d_array/RandomStudents.java
+++ src/d_array/RandomStudents.java
@@ -9,14 +9,26 @@ public class RandomStudents {
 		/*
 		 * 입력한 숫자만큼 우리반 학생들중 랜덤으로 뽑아서 출력하는 프로그램을 만들어주세요.
 		 */
-		String[] student = new String[]{"톰", "브라운", "아이작", "링컨", "뉴턴", "오라클", "나디아", "소피아", "피카츄", "지우"}; //10명의 학생 
+		String[] student = new String[]{"톰", "브라운", "아이작", "링컨", "뉴턴", "오라클", 
+				"나디아", "소피아", "피카츄", "지우"}; //10명의 학생 
 		
 		
 		Scanner s = new Scanner(System.in);		
-		System.out.println("학생 수를 임의로 입력 해주세요> ");
+		System.out.print("학생 수를 임의로 입력 해주세요> ");
+		int input = s.nextInt();
+		int[] number = new int[input];
+//		int count = (int)(Math.random()*student.length);
+		
+		
+		//반복문으로 student 인덱스를 검사하여 랜덤으로 이름을 뽑도록 하자.
+//		student[]	
+		for (int i = 0; i < input; i++) {
+			number[i] = (int) (Math.random() * student.length);
+
+			System.out.print(student[number[i]] + " ");
+		}
 
 		
-		
-	}
+	}// main블럭
 
 }
