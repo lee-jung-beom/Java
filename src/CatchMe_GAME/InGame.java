90958bd7134e54a1960f237b14d1ed1a7cbb612e
DESKTOP-0OOC6KV\PC-24 <academix128@gmail.com>
2021-04-01 21:44:03 +0900

    술래잡기 게임(90%)

--- src/CatchMe_GAME/InGame.java
+++ src/CatchMe_GAME/InGame.java
@@ -3,11 +3,15 @@ package CatchMe_GAME;
 import java.util.Arrays;
 
 public class InGame { //여기서 본 게임 프로세스 구성
+	
+	Player user = new Player("♧", 3, 1); //파라미터 : 이모티콘, 목숨, 레벨
+	Tagger tag = new Tagger("▣");
+	
 	Scan input = new Scan();
 
 	String[] stage1 = {"●","●","●","●","●","●","●","●","●","●"}; 
-	static String player = "♧";
-	static String tagger = "▣";
+//	static String player = "♧";
+//	static String tagger = "▣";
 	
 	void sumMethod(){ //이곳에 이 클래스에서 작성한 모든 메서드를 통합하여 간략하게 정리 해준다.
 		explainGame();
@@ -26,7 +30,9 @@ public class InGame { //여기서 본 게임 프로세스 구성
 	}
 	
 	
-	void Processing(){//☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆	
+	void Processing(){//☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+				
+		
 		int Rootine = 0; //while문을 위한 루틴(stage1에서 10회동안 술래를 피하면 게임 성공)
 		
 		String temp1;	
@@ -41,8 +47,9 @@ public class InGame { //여기서 본 게임 프로세스 구성
 		System.out.println(Arrays.toString(stage1)); //초기 stage 출력
 		System.out.println();
 		
-		while(Rootine < 10){ //플레이어와 술래의 이전 이동 정보를 저장하고, 다음 이동 입력 만큼 그곳에서 이동하도록하기(단, 이전 이동했을 때의 stage에 지정된 배열 정보는 지운다)	
-						
+		while(Rootine <= 10){ //플레이어와 술래의 이전 이동 정보를 저장하고, 다음 이동 입력 만큼 그곳에서 이동하도록하기(단, 이전 이동했을 때의 stage에 지정된 배열 정보는 지운다)	
+			Start startGame = new Start();
+			
 			playerRun += playerRun;
 			taggerRun += taggerRun;
 			
@@ -51,7 +58,7 @@ public class InGame { //여기서 본 게임 프로세스 구성
 			taggerRun = (int)((Math.random()*10+1)-1);
 			
 			
-			stage1[playerRun - 1] = player; //♧표시가 stage해당 배열에 저장
+			stage1[playerRun - 1] = user.player; //♧표시가 stage해당 배열에 저장
 			temp1 = stage1[playerRun - 1];
 			
 //			if(playerRun == 1){ //1을 입력했을 때는 player만 가도록 한다. (원활한 플레이를 위하여)
@@ -59,7 +66,7 @@ public class InGame { //여기서 본 게임 프로세스 구성
 //				continue;
 //			}
 			
-			stage1[taggerRun] = tagger; //▣표시가 stage해당 배열에 저장
+			stage1[taggerRun] = tag.tagger; //▣표시가 stage해당 배열에 저장
 			temp2 = stage1[taggerRun];
 
 //			if(taggerRun > playerRun){ //술래의 이동거리가 player보다 높게 선언되면 player의 이동거리와 같게 만든다.
@@ -69,17 +76,23 @@ public class InGame { //여기서 본 게임 프로세스 구성
 				System.out.println(Arrays.toString(stage1));
 				System.out.println("술래에게 잡혔습니다....ㅠㅠ");
 				Rootine = 0;
+				user.Life(); //목숨 메서드*****
+				System.out.println();
 				continue;
 			}			
 			System.out.println(Arrays.toString(stage1));
+			System.out.println();
 			
 			stage1[playerRun-1] = "●";
 			stage1[taggerRun] = "●";
 			
 			Rootine++;
-			if(Rootine == 9){
+			if(Rootine == 10){
+				System.out.println();
 				System.out.println("술래로부터 무사히 도망쳤습니다!!");
-				
+				user.Level();
+				System.out.println();
+				startGame.start(); //원래는 스테이지2를 제작하여야 함
 			}
 			
 		}//
@@ -89,34 +102,48 @@ public class InGame { //여기서 본 게임 프로세스 구성
 
 
 /*====================================================================================*/
-class Player extends InGame{ //player클래스 정의
-	Scan input = new Scan();
-	String[] temp = stage1;
-//	InGame app = new InGame();
+class Player{ //player클래스 정의
+//	Start start = new Start();
 	
-	String player; //player 식별 이모티콘 저장
-	
-	int movement; //player 이동 가능 거리
+	String player; //player 식별 이모티콘 저장	
 	int life; //목숨
-	int level;
+	int level; //레벨
 	
-	Player(int movement, int life, int level){
-		this.player = "♧";
+	Player(String player, int life, int level){
+		this.player = player;
 		this.life = life; //목숨 3개로 초기화
-//		this.movement = 3; //이동 가능 거리 파라미터 값 받아오기(player는 사용자가 값을 직접입력하여 이동할 수 있도록 한다. )
 		this.level = level; //초기 레벨 1로 선언
 	}	
+	
+	void showInfo(){
+		System.out.println("유저 이모티콘 : " + this.player);
+		System.out.println("생명 : " + this.life);
+		System.out.println("레벨 : " + this.level);
+	}
+	
+	void Life(){
+		this.life--; //술래에게 잡히면 생명 1감소
+		System.out.println("남은 목숨 : " + this.life);
+		System.out.println();
+		if(this.life == 0){
+			System.out.println("GAME OVER");
+		}
+	}
+	
+	void Level(){
+		this.level++; //스테이지 클리어하면 레벨 1 업
+		System.out.println("레벨 UP↑ => LEVEL" + this.level);
+	}
 }// Player클래스 end블럭
 
 
 /*====================================================================================*/
 class Tagger{ //술래 클래스 정의
 	
-	String tagger = "▣"; //player 식별 이모티콘 저장
-	int movment; //술래 이동 가능 거리
+	String tagger; //player 식별 이모티콘 저장
 	
-	Tagger(int movement){
-		this.movment = movement; //이동 가능 거리 파라미터 값 받아오기(술래는 파라미터 값으로 랜덤함수를 선언하도록 한다.)
+	Tagger(String tagger){
+		this.tagger = tagger; 
 	}	
 }// Tagger클래스 end블럭
 
